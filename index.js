1016844fbe3a568ea726dfd8755b9420488a9291
Amro dandashli <amrdandashli@gmail.com>
2021-04-27 17:07:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code here
+function saturdayFun(sleep="roller-skate") {
+  return `This Saturday, I want to ${sleep}!`
+};
+ function mondayWork(work="go to the office") {
+  `This Monday, I will ${work} .`
+};
